56a955a8671e5d513854b9684fe588a9ed244184
BradT <BradT@10.250.4.45>
2017-01-18 15:57:19 -0400

    My new Hello GitHub World statement

---
+++ src/w2017week2lab/W2017week2Lab.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package w2017week2lab;
+
+/**
+ *
+ * @author BradT
+ */
+public class W2017week2Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub world!!");
+        System.out.println("My GtiHub account name is Bradin8r_online");
+    }
+    
+}
